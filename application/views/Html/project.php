15841b49a45dfc2bfd4c4ddf6f2c7de91f31ee82
Mayasa25 <mayasa-89@hotmail.com>
2017-05-18 13:30:28 +0200

    the project page and the contactus

---
+++ application/views/Html/project.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Egret - HTML5 landing page</title>
+	
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   	<link rel="stylesheet" href="assets/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="assets/css/linea-icon.css" />
+	<link rel="stylesheet" href="assets/css/fancy-buttons.css" />
+	<link rel="stylesheet" type="text/css" href="assets/css/test.css">
+	
+	<!--=== Google Fonts ===-->
+	 <link  href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href='http://fonts.googleapis.com/css?family=Bangers' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:300,700,400' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Raleway:600,400,300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+	<!--=== Other CSS files ===-->
+	<link rel="stylesheet" href="assets/css/animate.css" />
+	<link rel="stylesheet" href="assets/css/jquery.vegas.css" />
+	<link rel="stylesheet" href="assets/css/baraja.css" />
+	<link rel="stylesheet" href="assets/css/jquery.bxslider.css" />
+	
+	<!--=== Main Stylesheets ===-->
+	<link rel="stylesheet" href="assets/css/style.css" />
+	<link rel="stylesheet" href="assets/css/responsive.css" />
+	
+	<!--=== Color Scheme, three colors are available red.css, orange.css and gray.css ===-->
+	<link rel="stylesheet" id="scheme-source" href="assets/css/schemes/gray.css" />
+	
+	<!--=== Internet explorer fix ===-->
+	<!-- [if lt IE 9]>
+		<script src="http://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="http://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif] -->
+	
+	
+</head>
+<body>
+<span class="mayasa">hello world </span>
+<!--=== Features section Starts ===-->
+	<section id="section-feature" class="feature-wrap">
+		<div class="container features center">
+			<div class="row">
+				<div class="col-lg-12">
+						<!--Features container Starts -->
+						<ul id="card-ul" class="features-hold baraja-container">					
+							<!-- Single Feature Starts -->
+							<li class="single-feature" title="Parallax Backgrounds">
+								<img src="assets/images/5.jpg" alt="" class="feature-image" /><!-- Feature Icon -->
+								<h4 class="feature-title color-scheme">Parallax Backgrounds</h4>
+								<p class="feature-text">
+									Curabitur posuere feugiat ipsum, sed elementum tortor maximus ut
+								</p>
+								<a href="#section1" class="fancy-button button-line btn-col small zoom">
+									Details
+									<span class="icon">
+										<i class="fa fa-leaf"></i>
+									</span>
+								</a>
+							</li>
+							<!-- Single Feature Ends -->
+							
+							<!-- Single Feature Starts -->
+							<li class="single-feature" title="Ajax contact form">
+								<img src="assets/images/2.jpg" alt="" class="feature-image" /><!-- Feature Icon -->
+								<h4 class="feature-title color-scheme">Ajax contact form</h4>
+								<p class="feature-text">
+									Curabitur posuere feugiat ipsum, sed elementum tortor maximus ut
+								</p>
+								<a href="#section2" class="fancy-button button-line btn-col small zoom">
+									Details
+									<span class="icon">
+										<i class="fa fa-leaf"></i>
+									</span>
+								</a>
+							</li>
+							<!-- Single Feature Ends -->
+							
+							<!-- Single Feature Starts -->
+							<li class="single-feature" title="unlimited Google fonts">
+								<img src="assets/images/3.jpg" alt="" class="feature-image" /><!-- Feature Icon -->
+								<h4 class="feature-title color-scheme">unlimited Google fonts</h4>
+								<p class="feature-text">
+									Curabitur posuere feugiat ipsum, sed elementum tortor maximus ut
+								</p>
+								<a href="#section3" class="fancy-button button-line btn-col small zoom">
+									Details
+									<span class="icon">
+										<i class="fa fa-leaf"></i>
+									</span>
+								</a>
+							</li>
+							<!-- Single Feature Ends -->
+							
+							<!-- Single Feature Starts -->
+							<li class="single-feature" title="Feature heading">
+								<img src="assets/images/4.JPG" alt="" class="feature-image" /><!-- Feature Icon -->
+								<h4 class="feature-title color-scheme">Feature heading</h4>
+								<p class="feature-text">
+									Curabitur posuere feugiat ipsum, sed elementum tortor maximus ut
+								</p>
+								<a href="#section4" class="fancy-button button-line btn-col small zoom">
+									Details
+									<span class="icon">
+										<i class="fa fa-leaf"></i>
+									</span>
+								</a>
+							</li>
+							<!-- Single Feature Ends -->
+						</ul>
+						<!--Features container Ends -->
+						
+						<!-- Features Controls Starts -->
+						<div class="features-control relative">
+							<button class="control-icon fancy-button button-line no-text btn-col bell" id="feature-prev" title="Previous" >
+								<span class="icon">
+									<i class="fa fa-arrow-left"></i>
+								</span>
+							</button>
+							<button class="control-icon fancy-button button-line no-text btn-col zoom" id="feature-expand" title="Expand" >
+								<span class="icon">
+									<i class="fa fa-expand"></i>
+								</span>
+							</button>
+							<button class="control-icon fancy-button button-line no-text btn-col zoom" id="feature-close" title="Collapse" >
+								<span class="icon">
+									<i class="fa fa-compress"></i>
+								</span>
+							</button>
+							<button class="control-icon fancy-button button-line no-text btn-col bell" id="feature-next" title="Next" >
+								<span class="icon">
+									<i class="fa fa-arrow-right"></i>
+								</span>
+							</button>
+						</div>
+
+						<!-- Features Controls Ends -->
+				</div>
+			</div>
+		</div>
+		<div id="section1">hello bla bla bla bla bla ala blab bla bala balab bla abalalablabalalal  abababalbalbalbkcgcbmbxckjzzhvljlvjl   ncllsdv  bsdkbdsjbc kbvkjfbjsbcx mxcbvkjbvkjcvnvkjdsvkc ksjdbvkjdsvkjsdkjvbdskjvkdjsb   b kskdbbdsvbsk  bsd bskdb ksdb bds jbbxcnLKdbs  bsliwiw sivbdisv s vsbdvubsijdvbsdlijvlsid djbvisdjvjsdkbvkjdsvsdj jsdbvjsdvskjskvhksjdhvjs jsdvhsjkvhsljdhvskjzjd djhskjhvksjhskjhvksdjvbcxkvbsd bdsb  bsijbsdjibksbvdskbvks s bdobvskdjbvskb dssssssssss sdjhvjbksdjbvksdbvksdbhello bla bla bla bla bla ala blab bla bala balab bla abalalablabalalal  abababalbalbalbkcgcbmbxckjzzhvljlvjl   ncllsdv  bsdkbdsjbc kbvkjfbjsbcx mxcbvkjbvkjcvnvkjdsvkc ksjdbvkjdsvkjsdkjvbdskjvkdjsb   b kskdbbdsvbsk  bsd bskdb ksdb bds jbbxcnLKdbs  bsliwiw sivbdisv s vsbdvubsijdvbsdlijvlsid djbvisdjvjsdkbvkjdsvsdj jsdbvjsdvskjskvhksjdhvjs jsdvhsjkvhsljdhvskjzjd djhskjhvksjhskjhvksdjvbcxkvbsd bdsb  bsijbsdjibksbvdskbvks s bdobvskdjbvskb dssssssssss sdjhvjbksdjbvksdbvksdb</div>
+		<div id="section2">hello bla bla bla bla bla ala blab bla bala balab bla abalalablabalalal  abababalbalbalbkcgcbmbxckjzzhvljlvjl   ncllsdv  bsdkbdsjbc kbvkjfbjsbcx mxcbvkjbvkjcvnvkjdsvkc ksjdbvkjdsvkjsdkjvbdskjvkdjsb   b kskdbbdsvbsk  bsd bskdb ksdb bds jbbxcnLKdbs  bsliwiw sivbdisv s vsbdvubsijdvbsdlijvlsid djbvisdjvjsdkbvkjdsvsdj jsdbvjsdvskjskvhksjdhvjs jsdvhsjkvhsljdhvskjzjd djhskjhvksjhskjhvksdjvbcxkvbsd bdsb  bsijbsdjibksbvdskbvks s bdobvskdjbvskb dssssssssss sdjhvjbksdjbvksdbvksdbhello bla bla bla bla bla ala blab bla bala balab bla abalalablabalalal  abababalbalbalbkcgcbmbxckjzzhvljlvjl   ncllsdv  bsdkbdsjbc kbvkjfbjsbcx mxcbvkjbvkjcvnvkjdsvkc ksjdbvkjdsvkjsdkjvbdskjvkdjsb   b kskdbbdsvbsk  bsd bskdb ksdb bds jbbxcnLKdbs  bsliwiw sivbdisv s vsbdvubsijdvbsdlijvlsid djbvisdjvjsdkbvkjdsvsdj jsdbvjsdvskjskvhksjdhvjs jsdvhsjkvhsljdhvskjzjd djhskjhvksjhskjhvksdjvbcxkvbsd bdsb  bsijbsdjibksbvdskbvks s bdobvskdjbvskb dssssssssss sdjhvjbksdjbvksdbvksdb</div>
+		<div id="section3">hello bla bla bla bla bla ala blab bla bala balab bla abalalablabalalal  abababalbalbalbkcgcbmbxckjzzhvljlvjl   ncllsdv  bsdkbdsjbc kbvkjfbjsbcx mxcbvkjbvkjcvnvkjdsvkc ksjdbvkjdsvkjsdkjvbdskjvkdjsb   b kskdbbdsvbsk  bsd bskdb ksdb bds jbbxcnLKdbs  bsliwiw sivbdisv s vsbdvubsijdvbsdlijvlsid djbvisdjvjsdkbvkjdsvsdj jsdbvjsdvskjskvhksjdhvjs jsdvhsjkvhsljdhvskjzjd djhskjhvksjhskjhvksdjvbcxkvbsd bdsb  bsijbsdjibksbvdskbvks s bdobvskdjbvskb dssssssssss sdjhvjbksdjbvksdbvksdbhello bla bla bla bla bla ala blab bla bala balab bla abalalablabalalal  abababalbalbalbkcgcbmbxckjzzhvljlvjl   ncllsdv  bsdkbdsjbc kbvkjfbjsbcx mxcbvkjbvkjcvnvkjdsvkc ksjdbvkjdsvkjsdkjvbdskjvkdjsb   b kskdbbdsvbsk  bsd bskdb ksdb bds jbbxcnLKdbs  bsliwiw sivbdisv s vsbdvubsijdvbsdlijvlsid djbvisdjvjsdkbvkjdsvsdj jsdbvjsdvskjskvhksjdhvjs jsdvhsjkvhsljdhvskjzjd djhskjhvksjhskjhvksdjvbcxkvbsd bdsb  bsijbsdjibksbvdskbvks s bdobvskdjbvskb dssssssssss sdjhvjbksdjbvksdbvksdb</div>
+		<div id="section4">hello bla bla bla bla bla ala blab bla bala balab bla abalalablabalalal  abababalbalbalbkcgcbmbxckjzzhvljlvjl   ncllsdv  bsdkbdsjbc kbvkjfbjsbcx mxcbvkjbvkjcvnvkjdsvkc ksjdbvkjdsvkjsdkjvbdskjvkdjsb   b kskdbbdsvbsk  bsd bskdb ksdb bds jbbxcnLKdbs  bsliwiw sivbdisv s vsbdvubsijdvbsdlijvlsid djbvisdjvjsdkbvkjdsvsdj jsdbvjsdvskjskvhksjdhvjs jsdvhsjkvhsljdhvskjzjd djhskjhvksjhskjhvksdjvbcxkvbsd bdsb  bsijbsdjibksbvdskbvks s bdobvskdjbvskb dssssssssss sdjhvjbksdjbvksdbvksdbhello bla bla bla bla bla ala blab bla bala balab bla abalalablabalalal  abababalbalbalbkcgcbmbxckjzzhvljlvjl   ncllsdv  bsdkbdsjbc kbvkjfbjsbcx mxcbvkjbvkjcvnvkjdsvkc ksjdbvkjdsvkjsdkjvbdskjvkdjsb   b kskdbbdsvbsk  bsd bskdb ksdb bds jbbxcnLKdbs  bsliwiw sivbdisv s vsbdvubsijdvbsdlijvlsid djbvisdjvjsdkbvkjdsvsdj jsdbvjsdvskjskvhksjdhvjs jsdvhsjkvhsljdhvskjzjd djhskjhvksjhskjhvksdjvbcxkvbsd bdsb  bsijbsdjibksbvdskbvks s bdobvskdjbvskb dssssssssss sdjhvjbksdjbvksdbvksdb</div>
+	</section>
+
+<!--=== Features section Ends ===-->
+<!--==== Essential files ====-->
+<script type="text/javascript" src="assets/js/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrapValidator.min.js"></script>
+<script type="text/javascript" src="assets/js/modernizr.js"></script>
+<script type="text/javascript" src="assets/js/jquery.easing.1.3.js"></script>
+
+<!--==== Slider and Card style plugin ====-->
+<script type="text/javascript" src="assets/js/jquery.baraja.js"></script>
+<script type="text/javascript" src="assets/js/jquery.vegas.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.bxslider.min.js"></script>
+
+<!--==== MailChimp Widget plugin ====-->
+<script type="text/javascript" src="assets/js/jquery.ajaxchimp.min.js"></script>
+
+<!--==== Scroll and navigation plugins ====-->
+<script type="text/javascript" src="assets/js/jquery.nicescroll.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.nav.js"></script>
+<script type="text/javascript" src="assets/js/jquery.appear.js"></script>
+<script type="text/javascript" src="assets/js/jquery.fitvids.js"></script>
+
+<!--==== Custom Script files ====-->
+<script type="text/javascript" src="assets/js/custom.js"></script>
+<script type="text/javascript" src="assets/js/show.js"></script>
+
+</body>
+</html>
